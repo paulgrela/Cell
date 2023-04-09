a0e455968b0189a439bede317f06ea2162114873
Paul Grela <paulgrela@interia.pl>
2023-04-09 15:37:54 +0200

    First working version for making random reaction with any number of reactants and products

--- CellEngineProject/CellEngineWellStirredChemicalReactionsSimulation.h
+++ CellEngineProject/CellEngineWellStirredChemicalReactionsSimulation.h
@@ -60,51 +60,67 @@ public:
         Reactions.insert(std::make_pair(ReactionParam.ReactantsStr, ReactionParam));
     }
 public:
-    void TryDoReaction()
+    void TryDoReaction(const UnsignedIntType NumberOfReactants)
     {
         LoggersManagerObject.Log(STREAM(endl << "REACTION" << endl));
 
         std::mt19937_64 mt64X{ std::random_device{}() };
         std::uniform_int_distribution<uint64_t> UniformDistributionObject1X_Uint64t(1, Particles.size());
-        UnsignedIntType RandomParticleType1 = UniformDistributionObject1X_Uint64t(mt64X);
-        UnsignedIntType RandomParticleType2 = UniformDistributionObject1X_Uint64t(mt64X);
-        LoggersManagerObject.Log(STREAM("Particle 1 = (" << to_string(RandomParticleType1) << " | " << Particles[RandomParticleType1].Symbol << ") Particle 2 = (" << to_string(RandomParticleType2) << " | " << Particles[RandomParticleType2].Symbol << ")"));
-        RandomParticleType1--;
-        RandomParticleType2--;
 
-        if (RandomParticleType1 != RandomParticleType2)
+        vector<UnsignedIntType> RandomParticlesTypes;
+        for (UnsignedIntType ReactantNumber = 1; ReactantNumber <= NumberOfReactants; ReactantNumber++)
         {
-            vector<string> ParticlesSymbolsForReactionToSort{ Particles[RandomParticleType1].Symbol, Particles[RandomParticleType2].Symbol };
+            RandomParticlesTypes.emplace_back(UniformDistributionObject1X_Uint64t(mt64X));
+            LoggersManagerObject.Log(STREAM("Particle Reactant " << to_string(ReactantNumber) << " (" << to_string(RandomParticlesTypes.back()) << ")"));
+            RandomParticlesTypes.back()--;
+        }
+        
+        sort(begin(RandomParticlesTypes), end(RandomParticlesTypes));
+        auto IteratorUnique = unique(begin(RandomParticlesTypes), end(RandomParticlesTypes));
+        if (IteratorUnique == RandomParticlesTypes.end())
+        {
+            vector<string> ParticlesSymbolsForReactionToSort;
+            ParticlesSymbolsForReactionToSort.reserve(10);
+            for (auto& RandomParticleType : RandomParticlesTypes)
+                ParticlesSymbolsForReactionToSort.emplace_back(Particles[RandomParticleType].Symbol);
             std::sort(ParticlesSymbolsForReactionToSort.begin(), ParticlesSymbolsForReactionToSort.end());
-            string ReactionSymbolsStr = ParticlesSymbolsForReactionToSort[0] + " + " + ParticlesSymbolsForReactionToSort[1];
 
+            string ReactionSymbolsStr;
+            for (auto& ParticleSymbolForReaction : ParticlesSymbolsForReactionToSort)
+                ReactionSymbolsStr += (ParticleSymbolForReaction + " + ");
             LoggersManagerObject.Log(STREAM("Reaction Symbols = [" << ReactionSymbolsStr << "]" << endl));
 
             auto ReactionIter = Reactions.find(ReactionSymbolsStr);
             if (ReactionIter != Reactions.end())
             {
-                LoggersManagerObject.Log(STREAM("Reaction parameters [ Reactant 0 Counter = " << ReactionIter->second.Reactants[0].Counter << " Particle Counter = " << Particles[RandomParticleType1].Counter << "]"));
-                LoggersManagerObject.Log(STREAM("Reaction parameters [ Reactant 1 Counter = " << ReactionIter->second.Reactants[1].Counter << " Particle Counter = " << Particles[RandomParticleType2].Counter << "]"));
-
                 bool IsPossible = true;
-                if (ReactionIter->second.Reactants[0].Counter > Particles[RandomParticleType1].Counter)
-                    IsPossible = false;
-                if (ReactionIter->second.Reactants[1].Counter > Particles[RandomParticleType2].Counter)
-                    IsPossible = false;
+
+                for (auto& ReactionReactant : ReactionIter->second.Reactants)
+                    if (ReactionReactant.Counter > Particles[ReactionReactant.Identifier].Counter)
+                    {
+                        IsPossible = false;
+                        break;
+                    }
+
                 if (IsPossible == true)
                 {
-                    LoggersManagerObject.Log(STREAM("BEFORE = (" << Particles[RandomParticleType1].Counter << " " << Particles[RandomParticleType2].Counter << ") "));
+                    for (auto& ReactionReactant : ReactionIter->second.Reactants)
+                        LoggersManagerObject.Log(STREAM("BEFORE REACTANT = " << ReactionReactant.Identifier << " " << Particles[ReactionReactant.Identifier].Counter));
+                    for (auto& ReactionProduct : ReactionIter->second.Products)
+                        LoggersManagerObject.Log(STREAM("BEFORE PRODUCT = " << ReactionProduct.Identifier << " " << Particles[ReactionProduct.Identifier].Counter));
+                    LoggersManagerObject.Log(STREAM(""));
 
-                    Particles[RandomParticleType1].Counter -=  ReactionIter->second.Reactants[0].Counter;
-                    Particles[RandomParticleType2].Counter -=  ReactionIter->second.Reactants[1].Counter;
+                    for (auto& ReactionReactant : ReactionIter->second.Reactants)
+                        Particles[ReactionReactant.Identifier].Counter -=  ReactionReactant.Counter;
 
                     for (auto& ReactionProduct : ReactionIter->second.Products)
                         Particles[ReactionProduct.Identifier].Counter +=  ReactionProduct.Counter;
 
-                    LoggersManagerObject.Log(STREAM("AFTER R1 = " << ReactionIter->second.Products[0].Identifier << " " << Particles[ReactionIter->second.Products[0].Identifier].Counter));
-                    LoggersManagerObject.Log(STREAM("AFTER R2 = " << ReactionIter->second.Products[1].Identifier << " " << Particles[ReactionIter->second.Products[1].Identifier].Counter));
-
-                    getchar();
+                    for (auto& ReactionReactant : ReactionIter->second.Reactants)
+                        LoggersManagerObject.Log(STREAM("AFTER REACTANT = " << ReactionReactant.Identifier << " " << Particles[ReactionReactant.Identifier].Counter));
+                    for (auto& ReactionProduct : ReactionIter->second.Products)
+                        LoggersManagerObject.Log(STREAM("AFTER PRODUCT = " << ReactionProduct.Identifier << " " << Particles[ReactionProduct.Identifier].Counter));
+                    LoggersManagerObject.Log(STREAM(""));
                 }
                 else
                     LoggersManagerObject.Log(STREAM("Particles types are the same!"));
@@ -113,7 +129,7 @@ public:
                 LoggersManagerObject.Log(STREAM("Reaction for particles does not exist!"));
         }
         else
-            LoggersManagerObject.Log(STREAM("Particles types for reaction are the same!"));
+            LoggersManagerObject.Log(STREAM("Particles types for reaction are not unique!"));
 
         LoggersManagerObject.Log(STREAM("END OF REACTION" << endl));
     }
@@ -121,7 +137,7 @@ public:
     void Run(UnsignedIntType NumberOfSteps)
     {
         for (UnsignedIntType Steps = 1; Steps <= NumberOfSteps; Steps++)
-            TryDoReaction();
+            TryDoReaction(2);
     }
 };
 
@@ -141,12 +157,12 @@ void TestWellStirredChemicalReactionsSimulation()
     sim.AddParticles({ 10, "Eten", "CH2CH2", false, 10 });
     sim.AddParticles({ 11, "Ethylene", "CH2CH2O", false, 10 });
 
-    sim.AddReaction(ReactionType("C6H12O6 + O6", { {2, 1}, {3, 6} }, { {4, 6}, {1, 6} }));
-    sim.AddReaction(ReactionType("CH2CH2 + H2O", { {5, 1}, {1, 1} }, { {6, 1} }));
-    sim.AddReaction(ReactionType("CH3CHCH2 + HX", { {7, 1}, {8, 1} }, { {9, 1} }));
-    sim.AddReaction(ReactionType("CH2CH2 + O", { {10, 1}, {3, 1} }, { {11, 1} }));
+    sim.AddReaction(ReactionType("C6H12O6 + O6 + ", { {2, 1}, {3, 6} }, { {4, 6}, {1, 6} }));
+    sim.AddReaction(ReactionType("CH2CH2 + H2O + ", { {5 - 1, 1}, {1 - 1, 1} }, { {6 - 1, 1} }));
+    sim.AddReaction(ReactionType("CH3CHCH2 + HX + ", { {7, 1}, {8, 1} }, { {9, 1} }));
+    sim.AddReaction(ReactionType("CH2CH2 + O + ", { {10, 1}, {3, 1} }, { {11, 1} }));
 
-    sim.Run(15);
+    sim.Run(1);
 }
 
 #endif
